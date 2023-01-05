3192e27f5a4f28a07799e8679c5e2f280d3802a3
franck <franckkoukougnon@yahoo.fr>
2023-01-05 10:58:07 +0100

    first commit

---
+++ loathis/src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
